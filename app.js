e49c53523bdff21e307d288fa2dc0b6b3bff0d24
AnaKarolina52 <karol.teatro@me.com>
2020-01-28 15:40:46 +0000

    ‘commit’

---
+++ app.js
@@ -0,0 +1,40 @@
+var firstName = 'AnaKarol';
+var favouriteColor = 'azul';
+var favoriteFood = 'cake';
+var city = 'Dublin';
+
+//show the value of the variable firstName
+console.log(firstName);
+//show the value of the variable favouriteColor
+console.log(favouriteColor)
+//show the value of the variable favoriteFood
+console.log(favoriteFood);
+//show the value of the variable city
+console.log(city);
+
+
+var x = 'Tic';
+x = 'Tac';
+x = 'Toe';
+
+//testing the x value
+//console.log(x);
+
+var x = 'Laurel';
+var y = 'Hardy';
+var z = 'y';
+y = 'x';
+x = 'z';
+//testing the y value
+//console.log(y);
+var phase = "A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.";
+var endingPhrase =" is what we see. Rainbow colours, are made with light bending you see!";
+var one = 'Red,';
+var two = 'Orange,';
+var three = 'Yellow,';
+var four = 'Green,';
+var five = 'Blue,';
+var six = 'Indigo,';
+var seven = 'Violet,';
+
+console.log(phase+one +two+three+four+five+six+seven+endingPhrase);
